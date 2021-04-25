c6b01920ecf4493b26790449535d8cc54702d9b3
Joaquín Fernández Campo <joaquin.fernandez.campo.next@bbva.com>
2021-04-25 12:05:18 +0200

    Added more tooling for sound and camera control

--- asdf/install.sh
+++ asdf/install.sh
@@ -13,7 +13,7 @@ asdf plugin add oc https://github.com/sqtran/asdf-oc.git
 asdf plugin-add rust https://github.com/code-lever/asdf-rust.git
 asdf plugin-add terraform 
 asdf plugin-add promtool https://github.com/looztra/asdf-promtool
-
+asdf plugin-add nodejs
 
 asdf install golang 1.13.15 1.16.3
 asdf install python 3.9.4
@@ -23,5 +23,6 @@ asdf install oc 4.7.7
 asdf install terraform 0.14.10
 asdf install promtool v2.26.0
 asdf install istioctl 1.9.3
+asdf install nodejs 16.0.0
 
 ln -sf "$( dirname "${canonical}")/.tool-versions" ~/.tool-versions
--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ htop docker zlib1g-dev build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
 xz-utils tk-dev libffi-dev liblzma-dev python-openssl git xclip bash-completion lastpass-cli \
 build-essential cmake libcurl4 libcurl4-openssl-dev libxml2 libxml2-dev libssl1.1 \
-pkg-config ca-certificates sshpass
+pkg-config ca-certificates sshpass pavucontrol v4l-utils
   
 
 grep -e "${USER}.*zsh" /etc/passwd -q || chsh -s "$(which zsh)"
@@ -17,6 +17,7 @@ ln -sf "$(pwd)/dotfiles/001-alias.zsh" ~/.oh-my-zsh/custom/
 grep -q asdf ~/.zshrc || echo ". \$HOME/.asdf/asdf.sh" >> ~/.zshrc
 dconf load /org/gnome/terminal/legacy/profiles:/ < terminator/config
 sudo snap install --classic code
+sudo snap install --classic slack
 sudo snap install spotify
 
 while read -r line; do
